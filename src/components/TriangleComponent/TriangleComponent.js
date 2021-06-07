bab19c5b0b4afc7e5ee6ec5a069a4febb4390a89
c-assl <cecile.asselin@etu.utc.fr>
2021-06-07 09:05:08 +0200

    1st SVG draft for first item of part 3 main visualization

--- src/components/TriangleComponent/TriangleComponent.js
+++ src/components/TriangleComponent/TriangleComponent.js
@@ -4,9 +4,11 @@ import get from 'axios';
 import { geoEqualEarth, geoPath } from "d3-geo";
 import { uniq } from 'lodash';
 import { scaleLinear } from 'd3-scale';
-import { extent } from 'd3-array';
+import { max } from 'd3-array';
 
 import { generatePalette } from '../../helpers/misc';
+
+import './TriangleComponent.css'
 
 const TriangleComponent = ({
   dataFilename,
@@ -51,23 +53,84 @@ const TriangleComponent = ({
     )
   }
 
+  const NUMBER_OF_COLUMNS = 5
+  // const columnWidth = width/data.length
+  const columnWidth = width / NUMBER_OF_COLUMNS
+  const rowHeight = height / 5
+
+  // scaleLinear<Range = number, Output = Range, Unknown = never>(range?: Iterable<Range>): ScaleLinear<Range, Output, Unknown> (+1 overload)
+  const scaleX = scaleLinear().domain([
+    0,
+    max(
+      data.map((port) => { // data.map : boucle fonctionelle
+        // return +port[xVariable];
+        return +port.nb_pointcalls_out;
+      })
+    )
+  ]).range([0, columnWidth]);
+
+  const scaleY = scaleLinear().domain([
+    0,
+    max(
+      data.map((port) => {
+        return +port.mean_tonnage; // parseFloat(port.mean_tonnage);
+      })
+    )
+  ]).range([0, rowHeight]);
+
+
+
   return (
     <div>
 
       <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} style={{ border: '1px solid lightgrey' }}>
-          {
-            data.map((port, index) => {
-              return (
-                <g 
-                  transform={`translate(${(index) * (width/data.length)}, ${height * .33 + (index%3)*(height/5)})`}
-                >
-                    <rect
-                        x = {0}
-                        y = {0}
-                    />
+        {
+          data.map((port, index) => {
 
-                    
-                        
+            const rectWidth = scaleX(+port.nb_pointcalls_out)
+            const rectHeight = scaleY(+port.mean_tonnage)
+
+            const xIndex = index % NUMBER_OF_COLUMNS;
+            const yIndex = (index - index % NUMBER_OF_COLUMNS) / NUMBER_OF_COLUMNS;
+            const xTransform = xIndex * columnWidth;
+            const yTransform = yIndex * rowHeight;
+            return (
+              <g
+                key={index}
+                // transform={`translate(${(index) * (columnWidth)}, ${height * .33 + (index%3)*(rowHeight)})`}
+                transform={`translate(${xTransform}, ${yTransform})`}
+              >
+
+                <rect
+                  x={0}
+                  y={0}
+                  width={columnWidth}
+                  height={rowHeight}
+                  fill="grey"
+                  stroke="white"
+                />
+
+                <path
+                  stroke='red'
+                  d={`M ${(columnWidth - rectWidth) / 2} ${0} 
+                        H ${(columnWidth - rectWidth) / 2 + rectWidth}
+                        L ${columnWidth / 2} ${rectHeight}
+                        Z
+                        `} // accents chelous : je commence à faire des interpolations 
+
+
+                />
+                <rect
+                  x={(columnWidth - rectWidth) / 2}
+                  y={0}
+                  width={rectWidth}
+                  height={rectHeight}
+                  fill='transparent'
+                  stroke='black'
+                />
+
+
+
                 {/* <path
                   key={`path-${i}`}
                   d={geoPath().projection(projection)(d)}
@@ -76,14 +139,16 @@ const TriangleComponent = ({
                   stroke="#FFFFFF"
                   strokeWidth={0.5}
                 /> */}
-                <text> {port.port} </text>
-                    
-                </g>
-              )
-            })
+                <text
+                  y={parseInt(rowHeight / 2)} // attention on veut un entier en coord
+                > {port.port} </text>
 
-          }
-        
+              </g>
+            )
+          })
+
+        }
+
       </svg>
 
     </div>

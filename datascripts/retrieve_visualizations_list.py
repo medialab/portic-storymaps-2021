2a8d9a7748cdd3a9dd8e48f5c1a0063bd8a68ad6
Robin de Mourat <robin.demourat@gmail.com>
2021-06-08 19:32:03 +0200

    add visualizations list retrieval script

---
+++ datascripts/retrieve_visualizations_list.py
@@ -0,0 +1,26 @@
+import requests
+import csv
+import json
+
+LIST_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR65herfDXGXCgMTJt020TqjIVnAyLiDB-IkcpShhd9rZysqAt0sMGvXOAHgItSkTKqvB1TWXMuDQZR/pub?output=csv'
+
+TARGET = '../src/visualizations.json'
+
+def get_online_csv(url):
+  """
+  Cette fonction permet de récupérer le contenu d'un csv en ligne.
+  Pour les google spreadsheets: fichier > publier sur le web > format csv > copier le lien
+  """
+  results = []
+  with requests.Session() as s:
+      download = s.get(url)
+      decoded_content = download.content.decode('utf-8')
+      reader = csv.DictReader(decoded_content.splitlines(), delimiter=',')
+      for row in reader:
+        results.append(row)
+  return results
+
+list = get_online_csv(LIST_URL)
+f = open(TARGET, "w")
+f.write(json.dumps(list))
+f.close()

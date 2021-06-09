1243e1ce842110bb9d33517eec80dea090c17efd
Robin de Mourat <robin.demourat@gmail.com>
2021-06-09 11:20:26 +0200

    display visualizations in atlas

--- src/pages/Atlas/Atlas.js
+++ src/pages/Atlas/Atlas.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
 import cx from 'classnames';
+
+import VisualizationContainer from '../../visualizations/index.js';
 // charger le json de la liste des visualisations de l'atlas
 import visualizations from '../../visualizationsList';
+
+const VisualizationFocus = ({visualization, lang}) => {
+  return (
+    <div className="VisualizationFocus">
+      <h2>Visualisation en lightbox : {visualization[`titre_${lang}`]}</h2>
+      <VisualizationContainer id={visualization.id} />
+    </div>
+  )
+}
 
 const visualizationsMap = visualizations.reduce((res, visualization) => ({
   ...res,
@@ -44,9 +55,7 @@ function Atlas({
     </ul>
     {
       shownVisualization ?
-      <div>
-        <h2>Visualisation en lightbox : {shownVisualization[`titre_${lang}`]}</h2>
-      </div>
+        <VisualizationFocus visualization={shownVisualization} lang={lang} />
       : null
     }
   </div>

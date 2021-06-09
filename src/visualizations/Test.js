07f6b0f7005e7f4c92faca4cfbd59d5b64aa9ac6
Robin de Mourat <robin.demourat@gmail.com>
2021-06-09 15:44:48 +0200

    clean data management poc

--- src/visualizations/Test.js
+++ src/visualizations/Test.js
@@ -1,7 +1,37 @@
 
 
-const Test = () => {
-  return(<div>Coucou, ceci est le composant de la visualisation avec l'id <code>test</code></div>)
+const Test = ({
+  datasets = {}
+}) => {
+  return(
+    <div className="Test">
+      <div>Coucou, ceci est le composant de la visualisation avec l'id <code>test</code></div>
+      {
+        Object.keys(datasets).length ? 
+        <div>
+          <h3>Datasets de la visualisation :</h3>
+          {
+            Object.entries(datasets)
+            .map(([datasetName, values]) => {
+              return (
+              <div key={datasetName}>
+                <h3>{datasetName}</h3>
+                <pre>
+                  <code>
+                    {
+                      JSON.stringify(values, null, 2)
+                    }
+                  </code>
+                </pre>
+              </div>
+              )
+            })
+          }
+        </div>
+        : null
+      }
+    </div>
+    )
 }
 
 export default Test;
--- src/visualizations/index.js
+++ src/visualizations/index.js
@@ -11,7 +11,6 @@ const VisualizationContainer = ({id, ...props}) => {
   const relevantDatasets = useMemo(() => {
     const viz = visualizationsList.find(v => v.id === id);
     if (viz) {
-      console.log('str datasets', viz.datasets)
       const datasetsIds = viz.datasets && viz.datasets.split(',').map(d => d.trim());
       if (datasetsIds.length && datasets) {
         return datasetsIds.reduce((cur, id) => ({
@@ -20,12 +19,11 @@ const VisualizationContainer = ({id, ...props}) => {
         }), {})
       }
     }
-  }, [id]);
-  console.log('relevantDatasets', relevantDatasets);
+  }, [id, datasets]);
   switch(id) {
     case 'test':
     default:
-      return <Test {...props} />
+      return <Test {...props} datasets={relevantDatasets || {}} />
   }
 }
 

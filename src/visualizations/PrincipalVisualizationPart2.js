8496324e6277a614718dbd725adf042bca5314fc
Robin de Mourat <robin.demourat@gmail.com>
2021-06-10 14:26:46 +0200

    add main visualizations architecture

---
+++ src/visualizations/PrincipalVisualizationPart2.js
@@ -0,0 +1,11 @@
+
+
+const PrincipalVisualizationPart2 = () => {
+  return (
+    <div>
+      Ceci est le composant de la visualisation principale n°2
+    </div>
+  )
+}
+
+export default PrincipalVisualizationPart2;

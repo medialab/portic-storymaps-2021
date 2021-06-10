226723dac07f81225add5fe278e95b4f16a01605
Robin de Mourat <robin.demourat@gmail.com>
2021-06-10 14:17:43 +0200

    update summary

--- src/summary.js
+++ src/summary.js
@@ -7,17 +7,20 @@ import testScrollyEn from '!babel-loader!mdx-loader!./contents/en/test-scrolly.m
 import Part1Fr from '!babel-loader!mdx-loader!./contents/fr/partie-1.mdx';
 import Part1En from '!babel-loader!mdx-loader!./contents/en/part-1.mdx';
 
-import Part2IntroFr from '!babel-loader!mdx-loader!./contents/fr/partie-2-intro.mdx';
+// import Part2IntroFr from '!babel-loader!mdx-loader!./contents/fr/partie-2-intro.mdx';
+// import Part2IntroEn from '!babel-loader!mdx-loader!./contents/en/part-2-intro.mdx';
+
+import Part2IntroFr from '!babel-loader!mdx-loader!./contents/fr/partie-2.mdx';
 import Part2IntroEn from '!babel-loader!mdx-loader!./contents/en/part-2-intro.mdx';
 
-import Part21Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-1.mdx';
-import Part21En from '!babel-loader!mdx-loader!./contents/en/part-2-1.mdx';
+// import Part21Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-1.mdx';
+// import Part21En from '!babel-loader!mdx-loader!./contents/en/part-2-1.mdx';
 
-import Part22Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-2.mdx';
-import Part22En from '!babel-loader!mdx-loader!./contents/en/part-2-2.mdx';
+// import Part22Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-2.mdx';
+// import Part22En from '!babel-loader!mdx-loader!./contents/en/part-2-2.mdx';
 
-import Part23Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-3.mdx';
-import Part23En from '!babel-loader!mdx-loader!./contents/en/part-2-3.mdx';
+// import Part23Fr from '!babel-loader!mdx-loader!./contents/fr/partie-2-3.mdx';
+// import Part23En from '!babel-loader!mdx-loader!./contents/en/part-2-3.mdx';
 
 import Part3Fr from '!babel-loader!mdx-loader!./contents/fr/partie-3.mdx';
 import Part3En from '!babel-loader!mdx-loader!./contents/en/part-3.mdx';
@@ -27,25 +30,25 @@ import AboutEn from '!babel-loader!mdx-loader!./contents/en/about.mdx';
 
 
 const summary = [
-  {
-    routes: {
-      fr: 'scrolly',
-      en: 'scrolly'
-    },
-    titles: {
-      fr: 'Tests scrollytelling',
-      en: 'Scrollytelling test'
-    },
-    contents: {
-      fr: 'fr/test-scrolly.mdx',
-      en: 'en/test-scrolly.mdx'
-    },
-    contentsProcessed: {
-      fr: TestScrollyFr,
-      en: testScrollyEn,
-    },
-    Component: ScrollyPage
-  },
+  // {
+  //   routes: {
+  //     fr: 'scrolly',
+  //     en: 'scrolly'
+  //   },
+  //   titles: {
+  //     fr: 'Tests scrollytelling',
+  //     en: 'Scrollytelling test'
+  //   },
+  //   contents: {
+  //     fr: 'fr/test-scrolly.mdx',
+  //     en: 'en/test-scrolly.mdx'
+  //   },
+  //   contentsProcessed: {
+  //     fr: TestScrollyFr,
+  //     en: testScrollyEn,
+  //   },
+  //   Component: ScrollyPage
+  // },
   {
     routes: {
       fr: 'partie-1',
@@ -63,6 +66,7 @@ const summary = [
       fr: Part1Fr,
       en: Part1En,
     },
+    Component: ScrollyPage
   },
   {
     routes: {
@@ -81,6 +85,7 @@ const summary = [
       fr: Part2IntroFr,
       en: Part2IntroEn,
     },
+    Component: ScrollyPage
   },
   // {
   //   routes: {
@@ -171,6 +176,7 @@ const summary = [
       fr: Part3Fr,
       en: Part3En,
     },
+    Component: ScrollyPage
   },
   {
     routes: {

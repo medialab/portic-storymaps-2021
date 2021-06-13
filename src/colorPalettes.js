8a26c2112a087869fa769be2fba8dc0568fc0b60
Robin de Mourat <robin.demourat@gmail.com>
2021-06-13 12:22:37 +0200

    externalize color palette

---
+++ src/colorPalettes.js
@@ -0,0 +1,30 @@
+
+
+const colorsPalettes = {
+  'product': {
+    'sel': '#E0CB0E',
+    'eau-de-vie et vins divers': '#A07BEE',
+    'autres produits': '#72808D',
+    'produit colonial (\'Café\', \'Sucre\', \'Indigo\', \'Coton non transformé\')': '#FEA43B',
+      },
+  'partner': {
+    'Afrique': '#875E2E',
+    'Reste du monde (USA)': '#E0E3E6',
+    'Grande Bretagne': '#542AAD',
+    'Europe': '#A07BEE',
+    'Ports francs et petites îles': '#5439F9',
+    'Colonies (Saint-Domingue, Indes, îles fr de l\'Amérique)': '#E5881A',
+    'Indéterminé (supposé réexportations vers Europe)': '#A07BEE'
+  },
+  'customs_office': {
+    'La Rochelle': '#41BEA3',
+    'Rochefort': '#349883',
+    'Saint-Martin-de-Ré': '#277262',
+    'Charente': '#5AD0F4',
+    'Aligre': '#67CBB6',
+    'Les Sables d\'Olonne': '#668EDB',
+    'Marennes': '#A7E6F9',
+  }
+}
+
+export default colorsPalettes;

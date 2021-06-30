1af69add5804e4f1fde74950ca7a35bc3ada9a26
c-assl <cecile.asselin@etu.utc.fr>
2021-06-30 15:46:51 +0200

    complete renderObject function for customs_offices : add the circle on the double triangle

--- src/visualizations/PrincipalVisualizationPart3/PrincipalVisualizationPart3.js
+++ src/visualizations/PrincipalVisualizationPart3/PrincipalVisualizationPart3.js
@@ -18,70 +18,133 @@ const renderLabel = (datum, x, y) => { // fonction à adapter pour donner le dou
 
 const renderStep3Object = (datum, x, y, { width }) => {
 
-    let sizeCoef = width * 0.05;
-    const totalTonnage = parseFloat(datum.else.cumulated_tonnage_in_region) + parseFloat(datum.else.cumulated_tonnage_out_region)
-    // console.log("tonnage total : ",totalTonnage)
+  let sizeCoef = width * 0.05;
+  const totalTonnage = parseFloat(datum.else.cumulated_tonnage_out_region) + parseFloat(datum.else.cumulated_tonnage_in_region)
+  // console.log("tonnage total : ",totalTonnage)
 
-    if (datum.else.type_of_object === "port") {
-      // définition des coefficients de taille des triangles en 3 classes distinctes (pour l'instant la définition des classes est expérimentale)
+  if (datum.else.type_of_object === "port") {
+    // définition des coefficients de taille des triangles en 3 classes distinctes (pour l'instant la définition des classes est expérimentale)
 
-      // se gérerait bien avec un switch
-      if (totalTonnage<1000){
-        sizeCoef = width * 0.025
-      }
-      else {
-        if (totalTonnage>5000){
-          sizeCoef = width * 0.075
-        }
+    // se gérerait bien avec un switch
+    if (totalTonnage < 1000) {
+      sizeCoef = width * 0.025
+    }
+    else {
+      if (totalTonnage > 5000) {
+        sizeCoef = width * 0.075
       }
     }
+  }
 
-    const leftTriangleHeight = parseFloat(datum.else.cumulated_tonnage_out_region) / totalTonnage * sizeCoef;
-    const rightTriangleHeight = parseFloat(datum.else.cumulated_tonnage_in_region) / totalTonnage * sizeCoef;
+  const leftTriangleHeight = parseFloat(datum.else.cumulated_tonnage_out_region) / totalTonnage * sizeCoef;
+  const rightTriangleHeight = parseFloat(datum.else.cumulated_tonnage_in_region) / totalTonnage * sizeCoef; // je pourrais déduire cette donnée de la taille du triangle gauche
 
-    return (
-      <g className='double-triangle' transform={`translate(${x},${y})`}>
+  let leftPath = null
+  let rightPath = null
 
-        <path className='left-triangle' fill='rgb(39, 129, 141)'
-          d={`M ${0} ${0}
+  if (datum.else.type_of_object === "customs_office") {
+
+    const totalValue = parseFloat(datum.else.cumulated_exports_value_from_region) + parseFloat(datum.else.cumulated_exports_value_from_ext)
+    const inPercentage = parseFloat(datum.else.cumulated_exports_value_from_region) / totalValue * 100
+    console.log("inPercentage ", datum.label," : ", inPercentage)
+
+    const partialCircle = require('svg-partial-circle')
+
+    let start = null
+    let end = null
+
+    // calcul des angles de départ et d'arrivée de l'arc de cercle, en fonction des données
+    start = Math.PI / 2 + (inPercentage - 50) / 100 * Math.PI
+    end = Math.PI * 3 / 2 - (inPercentage - 50) / 100 * Math.PI
+
+    leftPath = partialCircle(
+      0, 0,         // center X and Y
+      leftTriangleHeight + rightTriangleHeight,              // radius
+      start,          // start angle in radians --> Math.PI / 4
+      end             // end angle in radians --> Math.PI * 7 / 4
+    )
+      .map((command) => command.join(' '))
+      .join(' ')
+    // console.log(`<path d="${leftPath}" />`)
+
+    rightPath = partialCircle(
+      0, 0,             // center X and Y
+      leftTriangleHeight + rightTriangleHeight,                  // radius
+      start,              // start angle in radians --> Math.PI / 4
+      end - 2 * Math.PI   // end angle in radians --> Math.PI * 7 / 4
+    )
+      .map((command) => command.join(' '))
+      .join(' ')
+    // console.log(`<path d="${rightPath}" />`)
+
+  }
+
+  return (
+    <g className='double-triangle' transform={`translate(${x},${y})`}>
+
+      <path className='left-triangle' fill='rgb(39, 129, 141)'
+        d={`M ${0} ${0}
               V ${leftTriangleHeight / 2}
               L ${-Math.pow(5, 0.5) * leftTriangleHeight / 2} ${0}
               L ${0} ${-leftTriangleHeight / 2}
               Z
               `}
-        />
+      />
 
-        <path className='right-triangle' fill='rgb(44, 74, 156)'
-          d={`M ${0} ${0}
+      <path className='right-triangle' fill='rgb(44, 74, 156)'
+        d={`M ${0} ${0}
               V ${rightTriangleHeight / 2}
               L ${Math.pow(5, 0.5) * rightTriangleHeight / 2} ${0}
               L ${0} ${-rightTriangleHeight / 2}
               Z
               `}
-        />
-        <>
+      />
+      <>
         {
-          datum.else.type_of_object === "customs_office" ?
-          <>
-          <path 
-            d={`M ${0} ${0} 
-                C ${70} ${140}, ${110} ${140}, ${110} ${110}
-                `}  
-            stroke="black" fill="transparent"/>
+          // datum.else.type_of_object === "customs_office" ?
+          leftPath != null ?
 
-          <path 
-            d={`M ${0} ${0} 
-            C ${120} ${80}, ${180} ${80}, ${170} ${60}
-            `} 
-            stroke="red" fill="transparent"/>
-          </>
-          : null
+            <>
+              <text font-size="smaller"> {datum.label} </text>
+              <path
+                d={`${leftPath}
+                `}
+                stroke="blue"
+                stroke-width={width*0.005} // à ajuster en fonction de la largeur de l'écran
+                fill="transparent"
+              />
+
+              <path
+                d={`${rightPath}
+                `}
+                stroke="red"
+                stroke-width={width*0.005} // à ajuster en fonction de la largeur de l'écran
+                fill="transparent"
+              />
+            </>
+            :
+            null
         }
-        </>
-      </g>);
-  }
+      </>
+    </g>);
+}
 
 
+{/* <path
+d={`${leftPath}
+`}
+stroke="blue"
+stroke-width="10" // à ajuster en fonction de la largeur de l'écran
+fill="transparent"
+/>
+
+<path
+d={`${rightPath}
+`}
+stroke="red"
+stroke-width="10" // à ajuster en fonction de la largeur de l'écran
+fill="transparent"
+/> */}
 
 
 
@@ -91,76 +154,76 @@ const PrincipalVisualizationPart3 = ({ step, width, height }) => {
     <div className="PrincipalVisualizationPart3" height={height}>
       <div className={cx('step', { 'is-visible': step === 1 })}>
         {process.env.NODE_ENV === 'development' ?
-        <>
-          <GeoComponent
-            backgroundFilename="cartoweb_france_1789_geojson.geojson"
-            dataFilename="part_3_step1_viz_data.csv"
-            height={height * 0.7} // @TODO à changer quand je combin erais en un seul SVG ou component custom
-            label="port"
-            width={width} // j'aurais besoin de responsive
-            showLabels
-            centerOnRegion
-            rotationDegree={58}
-            renderObject={renderLabel}
-          // debug
-          />
-          <TriangleComponent
-            dataFilename="part_3_step1_viz_data.csv"
-            totalWidth={width} // @TODO adapter la height
-            numberOfColumns={25}
-            rowHeight={height * 0.3}
-          />
+          <>
+            <GeoComponent
+              backgroundFilename="cartoweb_france_1789_geojson.geojson"
+              dataFilename="part_3_step1_viz_data.csv"
+              height={height * 0.7} // @TODO à changer quand je combin erais en un seul SVG ou component custom
+              label="port"
+              width={width} // j'aurais besoin de responsive
+              showLabels
+              centerOnRegion
+              rotationDegree={58}
+              renderObject={renderLabel}
+            // debug
+            />
+            <TriangleComponent
+              dataFilename="part_3_step1_viz_data.csv"
+              totalWidth={width} // @TODO adapter la height
+              numberOfColumns={25}
+              rowHeight={height * 0.3}
+            />
           </>
           :
           <img alt="step-3.1" src={`${process.env.PUBLIC_URL}/maquettes/VIZ_3.1.svg`} />
-      }
+        }
       </div>
       <div className={cx('step', { 'is-visible': step === 2 })}>
-      {process.env.NODE_ENV === 'development' ?
-        <>
-        <SigmaComponent
-          data="toflit_aggregate_1789_only_out.gexf"
-          nodeColor={`admiralty`}
-          nodeSize={`inside_degree`}
-          labelDensity={0.5}
-        />
-        <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-0.png`} />
-        <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-1.png`} />
-        </>
-        :
-        <>
-        <SigmaComponent
-          data="toflit_aggregate_1789_only_out.gexf"
-          nodeColor={`admiralty`}
-          nodeSize={`inside_degree`}
-          labelDensity={0.5}
-        />
-          <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-0.png`} />
-          <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-1.png`} />
-        </>
-      
-      }
+        {process.env.NODE_ENV === 'development' ?
+          <>
+            <SigmaComponent
+              data="toflit_aggregate_1789_only_out.gexf"
+              nodeColor={`admiralty`}
+              nodeSize={`inside_degree`}
+              labelDensity={0.5}
+            />
+            <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-0.png`} />
+            <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-1.png`} />
+          </>
+          :
+          <>
+            <SigmaComponent
+              data="toflit_aggregate_1789_only_out.gexf"
+              nodeColor={`admiralty`}
+              nodeSize={`inside_degree`}
+              labelDensity={0.5}
+            />
+            <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-0.png`} />
+            <img alt="step-3.2" src={`${process.env.PUBLIC_URL}/maquettes/comparaison_centralite-1.png`} />
+          </>
+
+        }
       </div>
       <div className={cx('step', { 'is-visible': step === 3 })} height={height}>
-      {process.env.NODE_ENV === 'development' ?
-        <>
-        <GeoComponent
-          backgroundFilename="cartoweb_france_1789_geojson.geojson"
-          dataFilename="part_3_step3_viz_customs_offices_data.csv"
-          width={width}
-          height={height * 0.99}
-          // markerColor="type_of_object"
-          markerSize="type_of_object"
-          label="name"
-          showLabels
-          centerOnRegion
-          renderObject={renderStep3Object}
-        // debug="true"
-        />
-        </>
+        {process.env.NODE_ENV === 'development' ?
+          <>
+            <GeoComponent
+              backgroundFilename="cartoweb_france_1789_geojson.geojson"
+              dataFilename="part_3_step3_viz_customs_offices_data.csv"
+              width={width}
+              height={height * 0.99}
+              // markerColor="type_of_object"
+              markerSize="type_of_object"
+              label="name"
+              showLabels
+              centerOnRegion
+              renderObject={renderStep3Object}
+            // debug="true"
+            />
+          </>
           :
           <img alt="step-3.3" src={`${process.env.PUBLIC_URL}/maquettes/VIZ_3.3.svg`} />
-      }
+        }
       </div>
     </div>
   )

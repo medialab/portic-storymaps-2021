83776ddd47f4b50929b499660e158ae2795f2a69
Robin de Mourat <robin.demourat@gmail.com>
2021-07-05 11:47:11 +0200

    improve main title typography

--- src/contents/fr/metadata.js
+++ src/contents/fr/metadata.js
@@ -1,6 +1,6 @@
 /* eslint import/no-anonymous-default-export : 0 */
 export default {
   "title": "Commerce multi-échelles autour du port de La Rochelle au XVIIIème siècle",
-  "titleHTML": "Commerce multi-échelles autour du port de La Rochelle au XVIII<sup>ème</sup> siècle",
+  "titleHTML": "Commerce multi-échelles autour du port de La Rochelle au <span class=\"century\">XVIII</span><sup>e</sup> siècle",
   "subtitle": "Une étude de cas du programme de recherche interdisciplinaire PORTIC"
 }

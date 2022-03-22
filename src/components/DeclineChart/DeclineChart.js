710b2db046e7e4efbd8cb4067c0f901e8e681539
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-03-22 17:47:58 +0100

    translate viz-principale-partie-1

--- src/components/DeclineChart/DeclineChart.js
+++ src/components/DeclineChart/DeclineChart.js
@@ -5,6 +5,7 @@ import ProductsDistributionChart from './ProductsDistributionChart';
 import './DeclineChart.scss';
 import { fixSvgDimension } from '../../helpers/misc';
 
+import translate from '../../i18n/translate'
 
 /**
  * Configurable wrapper for main viz #1
@@ -34,68 +35,20 @@ const DeclineChart = (props) => {
   const height = atlasMode ? 1200 : fixSvgDimension(containerHeight);
   const width = fixSvgDimension(inputWidth);
   const messages = {
-    franceOverviewTitle: {
-      fr: (start, end) => `Évolution de la valeur des exports du royaume de France de ${start} à ${end}`,
-      en: (start, end) => `Evolution of the value of exports from the kingdom of France from ${start} to ${end}`
-    },
-    tradeEvolutionTitle: {
-      fr: (cityName, start, end) => `Évolution du commerce de la direction des fermes de ${cityName} de ${start} à ${end}`,
-      en: (cityName, start, end) => `Evolution of the direction des fermes of ${cityName} trade from ${start} to ${end}`
-    },
-    top90PctTitle: {
-      fr: (cityName, start, end) => `Comparaison des parts des produits exportés par la direction des fermes de ${cityName} totalisant plus de 90% du commerce en ${start} et en ${end} (ports francs non pris en compte)`,
-      en: (cityName, start, end) => `Comparison of shares of the top 90% of exported products by the direction des fermes of ${cityName} in ${start} and in ${end} (ports francs non pris en compte)`,
-    },
-    partInPct: {
-      fr: () => 'part des exports fr.',
-      en: () => 'share of french exports'
-    },
-    absoluteValue: {
-      fr: () => 'valeur absolue des exports',
-      en: () => 'exports\' absolute value'
-    },
-    herfindalLegendTitle: {
-      // fr: () => `Degré de diversité du commerce (inverse de l’indice de Herfindahl)`,
-      // en: () => `Degree of trade diversity (inverse of the Herfindahl index)`,
-      fr: () => `degré de diversité du commerce`,
-      en: () => `degree of trade diversity`,
-    },
-    herfindal0: {
-      fr: () => `commerce peu diversifié`,
-      en: () => `fewly diversified trade`,
-    },
-    herfindal1: {
-      fr: () => `commerce très diversifié`,
-      en: () => `very diversified trade`,
-    },
-    barTooltip: {
-      fr: (year, pct, city, herfindal) => `En <strong>${year}</strong>, ${pct}% des biens exportés depuis la France le sont à partir de <strong>${city}</strong>.<br/><br/>Concentration du commerce <i>(indice de Herfindahl-Hirschmann : somme du carré des parts du marché français par classe de produits exportés)</i> : <span>[colorBox] ${herfindal}</span>`,
-      en: (year, pct, city, herfindal) => `In <strong>${year}</strong>, ${pct}% of exported goods from France are exported <strong>${city}</strong>.<br/><br/>Trade concentration <i>(Herfindahl-Hirschmann index : sum of square french market' shares sorted by exported products classes)</i> : <span>[colorBox] ${herfindal}</span>`,
-    },
-    productTooltip: {
-      fr: (year, product, pct) => `En <strong>${year}</strong>, les produits de la classe "${product}" représentent <strong>${pct}%</strong> de la valeur des biens exportés par la direction des fermes de La Rochelle.`,
-      en: (year, product, pct) => `In <strong>${year}</strong>, products classed as "${product}" represent ${pct}% of goods exported by La Rochelle's direction des fermes.`,
-    },
-    sevenYearsWar: {
-      fr: () => 'guerre de sept ans',
-      en: () => 'seven years war'
-    },
-    austriaWar: {
-      fr: () => 'guerre franco-britannique au sein de la guerre de succession d’Autriche',
-      en: () => 'guerre franco-britannique au sein de la guerre de succession d’Autriche',
-    },
-    usIndependance: {
-      fr: () => 'guerre d’indépendance Américaine',
-      en: () => 'guerre d’indépendance Américaine',
-    },
-    laRochelleDiaspora: {
-      fr: () => 'Organisation de convois partant de La Rochelle pour échapper aux Anglais',
-      en: () => 'Organisation de convois partant de La Rochelle pour échapper aux Anglais',
-    },
-    // absolute1789: {
-    //   fr: item => `Total des exports en 1789 : ${(item.Exports / 1000000).toFixed(1)} m. de livres tournois`,
-    //   en: item => `Total des exports en 1789 : ${(item.Exports / 1000000).toFixed(1)} m. de livres tournois`
-    // }
+    franceOverviewTitle: (start, end) => translate('viz-principale-partie-1', 'franceOverviewTitle', lang, { start: start, end: end }),
+    tradeEvolutionTitle: (cityName, start, end) => translate('viz-principale-partie-1', 'tradeEvolutionTitle', lang, { cityName: cityName, start: start, end: end }),
+    top90PctTitle: (cityName, start, end) => translate('viz-principale-partie-1', 'top90PctTitle', lang, { cityName: cityName, start: start, end: end }),
+    partInPct: () => translate('viz-principale-partie-1', 'partInPct', lang),
+    absoluteValue: () => translate('viz-principale-partie-1', 'absoluteValue', lang),
+    herfindalLegendTitle: () => translate('viz-principale-partie-1', 'herfindalLegendTitle', lang),
+    herfindal0: () => translate('viz-principale-partie-1', 'herfindal0', lang),
+    herfindal1: () => translate('viz-principale-partie-1', 'herfindal1', lang),
+    barTooltip: (year, pct, city, herfindal) => translate('viz-principale-partie-1', 'barTooltip', lang, { year: year, pct: pct, city: city, herfindal: herfindal }),
+    productTooltip: (year, product, pct) => translate('viz-principale-partie-1', 'productTooltip', lang, { year: year, product: product, pct: pct }),
+    sevenYearsWar: (year, product, pct) => translate('viz-principale-partie-1', 'sevenYearsWar', lang, { year: year, product: product, pct: pct }),
+    austriaWar: () => translate('viz-principale-partie-1', 'austriaWar', lang),
+    usIndependance: () => translate('viz-principale-partie-1', 'usIndependance', lang),
+    laRochelleDiaspora: () => translate('viz-principale-partie-1', 'laRochelleDiaspora', lang)
   }
   const margins = { top: 10, right: 50, bottom: 30, left: 50 };
 
@@ -112,23 +65,23 @@ const DeclineChart = (props) => {
             height={fixSvgDimension(height/totalRows * rowFlex)}
             data={datasets['decline_longitudinal_data/decline_longitudinal_data.csv'].filter((d) => d.region === "France")}
             absoluteField="Exports"
-            title={messages.franceOverviewTitle[lang](startYear, endYear)}
+            title={messages.franceOverviewTitle(startYear, endYear)}
             axisLeftTitle={''}
-            axisRightTitle={messages.absoluteValue[lang]()}
+            axisRightTitle={messages.absoluteValue()}
             margins={margins}
             annotations={[
               {
                 type: 'span',
                 startYear: 1756,
                 endYear: 1763,
-                label: messages.sevenYearsWar[lang]()
+                label: messages.sevenYearsWar()
               },
               {
                 type: 'span',
                 startYear: 1744,
                 endYear: 1748,
                 row: .5,
-                label: messages.austriaWar[lang]()
+                label: messages.austriaWar()
               },
               {
                 type: 'span',
@@ -136,7 +89,7 @@ const DeclineChart = (props) => {
                 endYear: 1781,
                 labelPosition: 'left',
                 row: 3,
-                label: messages.usIndependance[lang]()
+                label: messages.usIndependance()
               },
 
               
@@ -162,11 +115,11 @@ const DeclineChart = (props) => {
             absoluteField="Exports"
             shareField="Exports_share"
             herfindhalField="product_revolutionempire_exports_herfindahl"
-            title={messages.tradeEvolutionTitle[lang]('La Rochelle', startYear, endYear)}
-            axisLeftTitle={messages.partInPct[lang]()}
-            axisRightTitle={messages.absoluteValue[lang]()}
+            title={messages.tradeEvolutionTitle('La Rochelle', startYear, endYear)}
+            axisLeftTitle={messages.partInPct()}
+            axisRightTitle={messages.absoluteValue()}
             margins={margins}
-            barTooltipFn={messages.barTooltip[lang]}
+            barTooltipFn={messages.barTooltip}
             cityName="La Rochelle"
             highlightYears={rows.comparison > 0 ? [startYear, endYear] : undefined}
             annotations={[
@@ -174,21 +127,21 @@ const DeclineChart = (props) => {
                 type: 'span',
                 startYear: 1756,
                 endYear: 1763,
-                label: messages.sevenYearsWar[lang]()
+                label: messages.sevenYearsWar()
               },
               {
                 type: 'span',
                 startYear: 1744,
                 endYear: 1748,
                 row: 1,
-                label: messages.austriaWar[lang]()
+                label: messages.austriaWar()
               },
               {
                 type: 'span',
                 startYear: 1747,
                 endYear: 1747,
                 row: 2,
-                label: messages.laRochelleDiaspora[lang]()
+                label: messages.laRochelleDiaspora()
               },
               {
                 type: 'span',
@@ -196,7 +149,7 @@ const DeclineChart = (props) => {
                 endYear: 1781,
                 labelPosition: 'left',
                 row: 3,
-                label: messages.usIndependance[lang]()
+                label: messages.usIndependance()
               },
               // {
               //   type: 'span',
@@ -208,9 +161,9 @@ const DeclineChart = (props) => {
               // },
             ]}
             colorScaleMessages={{
-              title: messages.herfindalLegendTitle[lang](),
-              minimum: messages.herfindal0[lang](),
-              maximum: messages.herfindal1[lang](),
+              title: messages.herfindalLegendTitle(),
+              minimum: messages.herfindal0(),
+              maximum: messages.herfindal1(),
             }}
             {
               ...{
@@ -232,10 +185,10 @@ const DeclineChart = (props) => {
             absoluteField="Exports"
             shareField="Exports_share"
             herfindhalField="product_revolutionempire_exports_herfindahl"
-            title={messages.tradeEvolutionTitle[lang]('Bordeaux', startYear, endYear)}
-            axisLeftTitle={messages.partInPct[lang]()}
-            axisRightTitle={messages.absoluteValue[lang]()}
-            barTooltipFn={ messages.barTooltip[lang]}
+            title={messages.tradeEvolutionTitle('Bordeaux', startYear, endYear)}
+            axisLeftTitle={messages.partInPct()}
+            axisRightTitle={messages.absoluteValue()}
+            barTooltipFn={ messages.barTooltip}
             cityName="Bordeaux"
             annotations={[
               {
@@ -243,14 +196,14 @@ const DeclineChart = (props) => {
                 startYear: 1756,
                 endYear: 1763,
                 row: 0.5,
-                label: messages.sevenYearsWar[lang]()
+                label: messages.sevenYearsWar()
               },
               {
                 type: 'span',
                 startYear: 1744,
                 endYear: 1748,
                 row: 0,
-                label: messages.austriaWar[lang]()
+                label: messages.austriaWar()
               },
               {
                 type: 'span',
@@ -258,14 +211,14 @@ const DeclineChart = (props) => {
                 endYear: 1781,
                 labelPosition: 'left',
                 row: 1,
-                label: messages.usIndependance[lang]()
+                label: messages.usIndependance()
               },
             ]}
             margins={margins}
             colorScaleMessages={{
-              title: messages.herfindalLegendTitle[lang](),
-              minimum: messages.herfindal0[lang](),
-              maximum: messages.herfindal1[lang](),
+              title: messages.herfindalLegendTitle(),
+              minimum: messages.herfindal0(),
+              maximum: messages.herfindal1(),
             }}
             {
               ...{
@@ -291,8 +244,8 @@ const DeclineChart = (props) => {
             years={[atlasMode ? 1750 : startYear, atlasMode ? 1789 : endYear]}
             margins={margins}
             herfindhalField="product_revolutionempire_exports_herfindahl"
-            title={messages.top90PctTitle[lang]('La Rochelle', atlasMode ? 1750 : startYear, atlasMode ? 1789 : endYear)}
-            productTooltipFn={messages.productTooltip[lang]}
+            title={messages.top90PctTitle('La Rochelle', atlasMode ? 1750 : startYear, atlasMode ? 1789 : endYear)}
+            productTooltipFn={messages.productTooltip}
             width={width}
           />
         )
